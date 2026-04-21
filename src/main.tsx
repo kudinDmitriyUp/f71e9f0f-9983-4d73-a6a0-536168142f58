7b8b86eb55484405034f6f39689e82ca249a2ba7
bender <kudin.dima@gmail.com>
2026-04-21 12:29:39 +0000

    Update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { initVisualEdit } from '@/utils/visual-edit'
+import Tag from "@/components/tag/Tag";
 
 initVisualEdit()
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       
+      <Tag />
       <App />
     </BrowserRouter>
   </StrictMode>,
